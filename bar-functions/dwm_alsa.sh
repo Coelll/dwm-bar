722d4660034288ca5b0f692a6e9a0925ba07d00f
Coel <92153446+Coelll@users.noreply.github.com>
2021-10-08 21:01:13 +0700

    Add files via upload

---
+++ bar-functions/dwm_alsa.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# A dwm_bar function to show the master volume of ALSA
+# Joe Standring <git@joestandring.com>
+# GNU GPLv3
+
+# Dependencies: alsa-utils
+
+dwm_alsa () {
+	STATUS=$(amixer sget Master | tail -n1 | sed -r "s/.*\[(.*)\]/\1/")
+    VOL=$(amixer get Master | tail -n1 | sed -r "s/.*\[(.*)%\].*/\1/")
+    printf "%s" "$SEP1"
+    if [ "$IDENTIFIER" = "" ]; then
+    	if [ "$STATUS" = "off" ]; then
+	            printf "🔇"
+    	else
+    		#removed this line becuase it may get confusing
+	        if [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
+	            printf "%s%% " "$VOL"
+	        elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
+	            printf "%s%% " "$VOL"
+	        else
+	            printf "%s%% " "$VOL"
+	        fi
+		fi
+    else
+    	if [ "$STATUS" = "off" ]; then
+    		printf " "
+    	else
+	        # removed this line because it may get confusing
+	        if [ "$VOL" -gt 0 ] && [ "$VOL" -le 33 ]; then
+	            printf " %s%% " "$VOL"
+	        elif [ "$VOL" -gt 33 ] && [ "$VOL" -le 66 ]; then
+	            printf " %s%% " "$VOL"
+	        else
+	            printf " %s%% " "$VOL"
+        	fi
+        fi
+    fi
+    printf "%s\n" "$SEP2"
+}
+
+dwm_alsa
